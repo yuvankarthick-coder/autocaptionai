1a70e851d1cd1096690ca79090731da1f59e82a6
yuvankarthick-coder <yuvankarthick072011@gmail.com>
2026-05-08 09:21:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ if uploaded_file:
 
     st.video("input.mp4")
 
-    if st.button("Generate Subtitles"):
+if st.button("Generate Subtitles"):
     output_path = generate_subtitled_video(video_path)
 
     import os
